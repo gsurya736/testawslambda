cbf12b2b9a40cae97f229439b6464871c37fbc85
gsurya736 <67796287+gsurya736@users.noreply.github.com>
2024-11-19 09:47:02 +0530

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,8 @@
+import json
+
+def lambda_handler(event, context):
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('cicd_pipeline_lambda_function_12test')
+    }
