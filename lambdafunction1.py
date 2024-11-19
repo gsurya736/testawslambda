871963cca9111d23ae9ce7321883e1e66f1f8cca
gsurya736 <67796287+gsurya736@users.noreply.github.com>
2024-11-19 09:52:41 +0530

    Update lambdafunction1.py

--- lambdafunction1.py
+++ lambdafunction1.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('cicd_pipeline_lambda_function_11')
+        'body': json.dumps('github test')
     }
